08ce67a035d0dacb8b9e434e32930f5b4761ab78
Bartek Błęcki <48883992+dwabe@users.noreply.github.com>
2019-05-07 14:01:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 'use strict';
 (function(){ 
+
+var showModal = function(event){event.preventDefault();
+document.querySelector('#modal-overlay').classList.add('show');
+var modalId = event.target.getAttribute('href');
+var modalCl = document.querySelector(modalId);
+modalCl.classList.add('show');
+};
 	
-	var showModal = function(event){
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.add('show');
-		var modalId = event.target.getAttribute('href');
-		var modalCl = document.querySelector(modalId);
-    modalCl.classList.add('show');
+var modalLinks = document.querySelectorAll('.show-modal');
+	
+for(var i = 0; i < modalLinks.length; i++){modalLinks[i].addEventListener('click', showModal);
+};
+	
+var hideModal = function(event){event.preventDefault();
+document.querySelector('.modal.show').classList.remove('show');
+document.querySelector('#modal-overlay').classList.remove('show');
+};
+	
+var closeButtons = document.querySelectorAll('.modal .close');
+	
+for(var i = 0; i < closeButtons.length; i++){closeButtons[i].addEventListener('click', hideModal);
 	};
 	
-	var modalLinks = document.querySelectorAll('.show-modal');
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 	
-	for(var i = 0; i < modalLinks.length; i++){
-		modalLinks[i].addEventListener('click', showModal);
-	}
+var modals = document.querySelectorAll('.modal');
 	
-	var hideModal = function(event){
-		event.preventDefault();
-		document.querySelector('.modal.show').classList.remove('show');
-		document.querySelector('#modal-overlay').classList.remove('show');
-		
-	};
-	
-	var closeButtons = document.querySelectorAll('.modal .close');
-	
-	for(var i = 0; i < closeButtons.length; i++){
-		closeButtons[i].addEventListener('click', hideModal);
-	}
-	
-	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-	
-	var modals = document.querySelectorAll('.modal');
-	
-	for(var i = 0; i < modals.length; i++){
-		modals[i].addEventListener('click', function(event){
+for(var i = 0; i < modals.length; i++){modals[i].addEventListener('click', function(event){
 			event.stopPropagation();
 		});
 	}

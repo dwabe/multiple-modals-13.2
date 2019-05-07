c9a659cd3816d33a4bc00460fc4e640224f8f09b
Bartek Błęcki <bartek@blecki.pl>
2019-05-07 13:40:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 'use strict';
 (function(){ 
-	/* W kodzie HTML i CSS dodaliśmy style dla prostego modala, który będzie zawsze wyśrodkowany w oknie. 
 	
-	Teraz wystarczy napisać funkcję otwierającą modal:
-	*/
 	
 	var showModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
 	};
 	
-	// Mimo, że obecnie mamy tylko jeden link, stosujemy kod dla wielu linków. W ten sposób nie będzie trzeba go zmieniać, kiedy zechcemy mieć więcej linków lub guzików otwierających modale
 	
 	var modalLinks = document.querySelectorAll('.show-modal');
 	
@@ -18,8 +14,7 @@
 		modalLinks[i].addEventListener('click', showModal);
 	}
 	
-	// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
-
+	
 	var hideModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
@@ -31,11 +26,9 @@
 		closeButtons[i].addEventListener('click', hideModal);
 	}
 	
-	// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
 	
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 	
-	// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
 	
 	var modals = document.querySelectorAll('.modal');
 	
@@ -44,11 +37,5 @@
 			event.stopPropagation();
 		});
 	}
-	
-	/* I to wszystko - mamy już działający modal! 
-	
-	ĆWICZENIE: 
-	Zmień funkcję showModal tak, aby w momencie wyświetlania była zmieniana treść nagłówka na dowolną inną, np. "Modal header". 
-	*/
 	
 })(); 
